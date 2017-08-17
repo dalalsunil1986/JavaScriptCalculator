a13939c8657c97c326f4dcfc0f5977fbb50e8b9b
ikismail <ikismail7@gmail.com>
2017-08-17 13:22:29 +0530

    completed Calculation

--- app.js
+++ app.js
@@ -5,33 +5,44 @@ var app = angular.module('myApp', []);
 
 app.controller('appController', ['$scope', function ($scope) {
 
-    $scope.data = "Hello World";
     $scope.value = "";
-    $scope.buttons = ["7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "/", "0", ".", "=", "+", "C"];
-    $scope.getData = function (btn) {
+    $scope.buttons = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "C"];
+    $scope.operators = ["*", "/", "-", "+"];
+    $scope.numbFun = function (btn) {
         if (btn == "C") {
             $scope.value = "";
-        }
-        else if (btn == "+") {
-            $scope.value1 = $scope.value;
-            $scope.reset();
-        } else if (btn == "-") {
-            $scope.value1 = $scope.value + "-";
-            $scope.reset();
-        } else if (btn == "=") {
-            $scope.value2 = $scope.value;
-            $scope.reset();
-            console.log('Value1', $scope.value1)
-            console.log('Value2', $scope.value2)
-            console.log('parseInt Value1', parseInt($scope.value1))
-            $scope.value = parseInt($scope.value1) + parseInt($scope.value2);
-        }
-        else {
+        } else {
             $scope.value += btn;
         }
     };
+    $scope.optFun = function (opt) {
+        $scope.opt = opt;
+        $scope.num1 = $scope.value;
+        $scope.reset();
+    };
 
+    $scope.equals = function () {
+        $scope.num2 = $scope.value;
+        $scope.value = $scope.calculate(parseInt($scope.num1), parseInt($scope.num2), $scope.opt);
+    };
     $scope.reset = function () {
         $scope.value = "";
+    };
+
+    $scope.calculate = function (num1, num2, opt) {
+        switch (opt) {
+            case "+" :
+                console.log('Add : ', num1 + num2);
+                return num1 + num2;
+            case "-" :
+                console.log('Sub : ', num1 - num2);
+                return num1 - num2;
+            case "*":
+                console.log('Mul : ', num1 * num2);
+                return num1 * num2;
+            case "/":
+                console.log('Div : ', num1 / num2);
+                return num1 / num2;
+        }
     }
 }]);
