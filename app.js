c6ad326fe2a43af4e990a29f69f97159259c328e
ikismail <ikismail7@gmail.com>
2017-08-17 12:11:48 +0530

    updation

--- app.js
+++ app.js
@@ -6,36 +6,32 @@ var app = angular.module('myApp', []);
 app.controller('appController', ['$scope', function ($scope) {
 
     $scope.data = "Hello World";
-
-    $scope.buttons = ["7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "/", "0", ".", "=", "+", "C"];
-
-
     $scope.value = "";
-
+    $scope.buttons = ["7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "/", "0", ".", "=", "+", "C"];
     $scope.getData = function (btn) {
-        var value = function () {
-            this.value1 = 0;
-            this.value2 = 0;
-        };
-
-        var newValue = new value();
-
         if (btn == "C") {
             $scope.value = "";
         }
         else if (btn == "+") {
-            newValue.value1 = $scope.value;
-            console.log('Value1', newValue.value1);
-            $scope.value = "";
+            $scope.value1 = $scope.value;
+            $scope.reset();
+        } else if (btn == "-") {
+            $scope.value1 = $scope.value + "-";
+            $scope.reset();
         } else if (btn == "=") {
-            newValue.value2 = $scope.value;
-            console.log('Value1', newValue.value1);
-            console.log('Value2', newValue.value2);
-            $scope.value = "";
-            $scope.value = newValue.value1 + newValue.value2;
+            $scope.value2 = $scope.value;
+            $scope.reset();
+            console.log('Value1', $scope.value1)
+            console.log('Value2', $scope.value2)
+            console.log('parseInt Value1', parseInt($scope.value1))
+            $scope.value = parseInt($scope.value1) + parseInt($scope.value2);
         }
         else {
             $scope.value += btn;
         }
+    };
+
+    $scope.reset = function () {
+        $scope.value = "";
     }
 }]);
